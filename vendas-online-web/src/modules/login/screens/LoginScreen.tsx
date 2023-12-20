3f7454bf43aeefbf3d3bd0cd20cf4df1dff90e21
Cleitonjr-dev <cleitonjr.dev@gmail.com>
2023-12-20 14:50:50 -0300

    feat: login

--- vendas-online-web/src/modules/login/screens/LoginScreen.tsx
+++ vendas-online-web/src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,12 @@
-import { BackgroundImage } from '../styles/loginScreen.styles';
+import { BackgroundImage, ContainerLogin, LogoImage } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
   return (
     <div>
       <BackgroundImage src="./background.png" />
+      <ContainerLogin>
+        <LogoImage src="./logo.png" />
+      </ContainerLogin>
     </div>
   );
 };
