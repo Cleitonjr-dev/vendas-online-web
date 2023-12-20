a920db4b39923a9830fa366948ff0649e78714f6
Cleitonjr-dev <cleitonjr.dev@gmail.com>
2023-12-20 17:43:56 -0300

    continue

--- vendas-online-web/src/modules/login/screens/LoginScreen.tsx
+++ vendas-online-web/src/modules/login/screens/LoginScreen.tsx
@@ -1,11 +1,19 @@
-import { BackgroundImage, ContainerLogin, LogoImage } from '../styles/loginScreen.styles';
+import {
+  BackgroundImage,
+  ContainerLogin,
+  LimitedContainer,
+  LogoImage,
+} from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
   return (
     <div>
       <BackgroundImage src="./background.png" />
+
       <ContainerLogin>
-        <LogoImage src="./logo.png" />
+        <LimitedContainer>
+          <LogoImage src="./logo.png" />
+        </LimitedContainer>
       </ContainerLogin>
     </div>
   );
